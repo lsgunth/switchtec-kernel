78eebb6b5d49c85a9acb0deda1c4688b8e39048e
Logan Gunthorpe <logang@deltatee.com>
2019-02-13 10:05:51 -0700

    Introduce Generation Variable
    Add a generation variable passed through the device id table.
    Will add generation 4 and other devices after they are functionally
    supported.
    
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>

--- linux/switchtec.h
+++ linux/switchtec.h
@@ -45,6 +45,13 @@ enum {
 	SWITCHTEC_GAS_PFF_CSR_OFFSET    = 0x134000,
 };
 
+enum switchtec_gen {
+	SWITCHTEC_GEN3,
+	SWITCHTEC_GEN3_MR4,
+	SWITCHTEC_GEN4,
+	SWITCHTEC_GEN4_ALPHA,
+};
+
 struct mrpc_regs {
 	u8 input_data[SWITCHTEC_MRPC_PAYLOAD_SIZE];
 	u8 output_data[SWITCHTEC_MRPC_PAYLOAD_SIZE];
@@ -382,6 +389,8 @@ struct switchtec_dev {
 	struct pci_dev *pdev;
 	struct device dev;
 	struct cdev cdev;
+
+	enum switchtec_gen gen;
 
 	int partition;
 	int partition_count;
